9f08cdfe2c9d91439f8c366300c0d22edfe07c06
Najath Lawani <najathlawani73@gmail.com>
2024-06-23 21:31:25 +0000

    Refactor

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from dotenv import dotenv_values
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+import certifi
+
+from models.pharmacy import Pharmacy
+from models.product import Product
+from models.user import User
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+async def init_db():
+    # Load the MongoDB connection string from the environment variable MONGODB_URI
+
+    CONNECTION_STRING = os.getenv('DB_URL')
+    print(CONNECTION_STRING)
+
+    # Create a MongoDB client
+    client = AsyncIOMotorClient(CONNECTION_STRING, tlsCAFile=certifi.where())
+
+    await init_beanie(database=client.pharmaDB, document_models=[Product, Pharmacy, User])
