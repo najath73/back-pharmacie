903ce4f6f13d94f2150f5b26d47fe1d8a5d3c967
Najath Lawani <najathlawani73@gmail.com>
2024-08-18 22:08:07 +0000

    ajout de patch

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PATCH","PUT", "DELETE"],
     allow_headers=["*"],
 )
 
