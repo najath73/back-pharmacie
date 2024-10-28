dc07f1a1f9b94466f7ffd6bce2f1f177914ada6c
Imrane Lawani <naimlawani01@gmail.com>
2024-10-28 11:12:30 +0100

    fix: with new model

--- models/product.py
+++ models/product.py
@@ -3,31 +3,51 @@ from beanie import Document, Link
 from pydantic import BaseModel
 from models.pharmacy import Pharmacy
 
-
-
+class Category(Document):
+    name: str
+    description: str
 class Product(Document):
     name: str
     description: str
     img: str
-    quantity: float
+    stock: float
     price: float
     pharmacy: Link[Pharmacy]
+    prescription: bool = False
+    category: Link[Category]
     class Settings:
         name = "products"
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": "13HJJER8HH!888U",
+                "name": "Product",
+                "description": "This is a product",
+                "img": "https://example.com/image.jpg",
+                "quantity": 10,
+                "price": 10.0,
+                "pharmacy": "13HJJER8HH!888U",
+                "prescription": False,
+                "category": "13HJJER8HH!888U"
+            }
+        }
         
 class UpdateProduct(BaseModel): 
     name: Optional [str] = None
     description: Optional[str] = None
     img: Optional[str] = None
-    quantity: Optional[float]= None
+    stock: Optional[float]= None
     price: Optional[float]= None
     pharmacy_id: Optional[str] = None
-
+    prescription: Optional[bool] = False
+    category_id: Optional[str] = None
 
 class AddProduct(BaseModel):
     name: str
     description: str = "Description"
-    img: Optional[str]
-    quantity: float
+    img: Optional[str] = None
+    stock: float
     price: float
     pharmacy_id: str
+    prescription: Optional[bool] = False
+    category_id: Optional[str] = None
--- routers/pharmacy.py
+++ routers/pharmacy.py
@@ -1,155 +1,148 @@
 from typing import List
 from bson import ObjectId
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pymongo.errors import DuplicateKeyError
-from models.pharmacy import Pharmacy, ProductInPharmacyAdd, PharmacyUpdated,ProductInPharmacyUpdate
+from models.pharmacy import Pharmacy, PharmacyUpdated
 from models.product import Product
-from models.user import PostUserToPharmacy, User
-from utils.auth import get_password_hash, generate_password
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from models.user import PharmacyEmploye, PharmacyUserCreate, User, UserCreate, UserRole
+from routers.user import create_user
+from utils.auth import  generate_password, get_current_user, get_password_hash
+from utils.email import send_registration_email
+from utils.services import fetch_pharmacy_by_id
+import logging
 import os
 from dotenv import load_dotenv
 
-from utils.email import send_registration_email
-
+# Load environment variables from a .env file
 load_dotenv()
 
-
+# Initialize the API router with a prefix and tags
 router = APIRouter(prefix="/pharmacies", tags=["Pharmacies"])
 
-#Get all pharmacy
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @router.get("", status_code=200)
-async def get_all_pharmacies() -> List [Pharmacy]:
-    pharmacy= await Pharmacy.find_all().to_list()
-    return pharmacy
+async def get_all_pharmacies() -> List[Pharmacy]:
+   """
+   Fetch all pharmacies from the database.
+   """
+   logger.info("Fetching all pharmacies")
+   return await Pharmacy.find_all().to_list()
 
-#Post a pharmacy
+# Add a new pharmacy to the database
 @router.post("", status_code=201, response_model=dict)
-async def post_a_pharmacy(payload: Pharmacy):
-   try:      
-      pharmacy_added= await payload.create()
+async def post_a_pharmacy(payload: Pharmacy, current_user: User = Depends(get_current_user)):
+   """
+   Add a new pharmacy to the database.
+   """
+   if current_user.role != UserRole.SUPER_ADMIN:
+      logger.error("Unauthorized access attempt by user: {current_user.username}")
+      raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
+   try:
+      logger.info(f"Adding a new pharmacy with name: {payload.name}")
+      pharmacy_added = await payload.create()
    except DuplicateKeyError as e:
-        error_details = e.details
-        error_message = error_details.get('errmsg', str(e))
-
-        error_info = {
-            "error_description": "Duplicate entry detected",
-            "error_message": error_message          
-        }
-        raise HTTPException(status_code=400, detail=error_info)
-      
+      error_message = e.details.get('errmsg', str(e))
+      logger.error(f"Duplicate entry detected: {error_message}")
+      raise HTTPException(status_code=400, detail={
+         "error_description": "Duplicate entry detected",
+         "error_message": error_message
+      })
    return {"message": "Pharmacy added successfully", "id": str(pharmacy_added.id)}
 
-#Get pharmacy by id
-@router.get("/{pharmacy_id}",status_code=200)
-async def get_pharmacy_by_id(pharmacy_id: str) ->Pharmacy:
-    pharmacy= await Pharmacy.get(pharmacy_id)
-    return pharmacy
+# Fetch a pharmacy by its ID
+@router.get("/{pharmacy_id}", status_code=200)
+async def get_pharmacy_by_id(pharmacy_id: str) -> Pharmacy:
+   """
+   Fetch a pharmacy by its ID.
+   """
+   logger.info(f"Fetching pharmacy with id: {pharmacy_id}")
+   pharmacy = await fetch_pharmacy_by_id(pharmacy_id)
+   return pharmacy
 
-#Update pharmacy
-@router.patch("/{pharmacy_id}",status_code=204)
-async def update_pharmacy(pharmacy_id: str , payload: PharmacyUpdated):
-   pharmacy_updated= await Pharmacy.get(pharmacy_id)
-   if (payload.name):
-       pharmacy_updated.name= payload.name
-       
-   if (payload.address):
-       pharmacy_updated.address= payload.address
-       
-   if (payload.phone):
-       pharmacy_updated.phone= payload.phone
+# Update an existing pharmacy's details
+@router.patch("/{pharmacy_id}", status_code=204)
+async def update_pharmacy(pharmacy_id: str, payload: PharmacyUpdated, current_user: User = Depends(get_current_user)):
+   """
+   Update an existing pharmacy's details.
+   """
+   logger.info(f"role: {current_user.role}")
+   if current_user.role == UserRole.CUSTOMER or current_user.role == UserRole.PHARMACY_EMPLOYEE:
+      logger.error(f"Unauthorized access attempt by user: {current_user.username}")
+      raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
+   
+   if current_user.role == UserRole.PHARMACY_ADMIN:
+      employeInfo = await PharmacyEmploye.find_one(PharmacyEmploye.user.id == ObjectId(current_user.id), fetch_links=True)
+      if employeInfo.pharmacy.id != ObjectId(pharmacy_id):
+         print(employeInfo.pharmacy.id)
+         logger.error(f"Unauthorized access attempt by user: {current_user.username}")
+         raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
+   logger.info(f"Updating pharmacy with id: {pharmacy_id}")
+   pharmacy = await fetch_pharmacy_by_id(pharmacy_id)
+   update_data = {key: value for key, value in payload.dict().items() if value is not None}
+   await pharmacy.update({"$set": update_data})
 
-   if (payload.img):
-       pharmacy_updated.img= payload.img
-       
-   if (payload.localisation):
-       pharmacy_updated.localisation= payload.localisation   
-
-      
-   await pharmacy_updated.save()
-   return 
-
-#delete pharmacy 
+# Delete a pharmacy by its ID
 @router.delete("/{pharmacy_id}", status_code=204)
-async def delete_pharmacy(pharmacy_id: str):
-   pharmacy_deleted= await Pharmacy.get(pharmacy_id)
-   await pharmacy_deleted.delete()
-   return 
+async def delete_pharmacy(pharmacy_id: str, current_user: User = Depends(get_current_user)):
+   """
+   Delete a pharmacy by its ID.
+   """
+   logger.info(f"role: {current_user.role}")
+   if current_user.role != UserRole.SUPER_ADMIN:
+      logger.error(f"Unauthorized access attempt by user: {current_user.username}")
+      raise HTTPException(status_code=403, detail="You do not have permission to perform this action")
+   logger.info(f"Deleting pharmacy with id: {pharmacy_id}")
+   pharmacy = await fetch_pharmacy_by_id(pharmacy_id)
+   await pharmacy.delete()
 
-#Function to verify that the product and pharmacy exist
-async def check_pharmacy_and_product(pharmacy_id, product_id):
-   
-   #get pharmacy
-   pharmacy= await Pharmacy.get(pharmacy_id)
-   if not pharmacy:
-      raise HTTPException(status_code=404, detail= "Pharmacy not found")
-   
-   #get product
-   product= await Product.get(product_id)
-   if not product:
-      raise HTTPException(status_code=404, detail="Product not found")
-   return
-   
-   
-    
-#Get all product in a pharmacy
-@router.get("/{pharmacy_id}/products", status_code=200)
-async def get_all_products_in_pharmacy(pharmacy_id: str) -> List [Product] :
-   pharmacy= await Pharmacy.get(pharmacy_id)
-   if not pharmacy:
-      raise HTTPException(status_code=404, detail= "Pharmacy not found")
-   
-   productsInPharmacy= pharmacy= await Product.find(Product.pharmacy.id == ObjectId(pharmacy_id),fetch_links=True).to_list()
-   return productsInPharmacy
-    
-          
-    
-#post a user to a pharmacy
-@router.post("/{pharmacy_id}/users",status_code=201, response_model=dict)
-async def post_user_to_a_pharmacy(pharmacy_id: str, payload: PostUserToPharmacy):
+# Add a new product to a specific pharmacy
+@router.get("/{pharmacy_id}/products", status_code=200, response_model=List[Product])
+async def get_all_products_in_pharmacy(pharmacy_id: str) -> List[Product]:
+   """
+   Fetch all products for a specific pharmacy.
+   """
+   logger.info(f"Fetching all products for pharmacy with id: {pharmacy_id}")
+   pharmacy = await fetch_pharmacy_by_id(pharmacy_id)
+   return await Product.find(Product.pharmacy.id == ObjectId(pharmacy_id), fetch_links=True).to_list()
 
-   payload.password = generate_password()
-   pswd_not_hash = (payload.password)
-   payload.password= get_password_hash(payload.password)
+# Fetch all users for a specific pharmacy
+@router.get("/{pharmacy_id}/users", status_code=200, response_model=List[PharmacyEmploye])
+async def get_user_for_a_pharmacy(pharmacy_id: str, current_user: User = Depends(get_current_user)):
+   """
+   Fetch all users for a specific pharmacy.
+   """
+   pharmacy = await fetch_pharmacy_by_id(pharmacy_id)
+   print(pharmacy.id)
+   return await PharmacyEmploye.find(PharmacyEmploye.pharmacy.id == pharmacy.id, fetch_links= True).to_list()
+
+
+# Add a user to a specific pharmacy
+@router.post("/{pharmacy_id}/users", status_code=201, response_model=dict)
+async def add_user_to_pharmacy(pharmacy_id: str, payload: PharmacyUserCreate):
+   """
+   Add a new user to a specific pharmacy.
+   """
+   # Validate email address
+   if "@" not in payload.email:
+      raise HTTPException(status_code=400, detail="Invalid email address")
+
+   # Call the create_user function to create a new user
    
-   user_to_create = User(
+   user_payload = UserCreate(
       username=payload.username,
-      name=payload.name,
-      firstname=payload.firstname,
       email=payload.email,
-      password=payload.password,
-      roles=payload.roles,
-      pharmacy=pharmacy_id  # Associe l'utilisateur à la pharmacie par l'ID
+      password=get_password_hash(payload.password),
+      role=payload.role,
+      first_name=payload.first_name,
+      last_name=payload.last_name,
+      pharmacy_id=pharmacy_id
    )
+   print(user_payload)
+   new_user = await create_user(user_payload)
+   # Send registration email to the new user
+   send_registration_email(payload.email, payload.username, payload.password)
+   return {"message": "User added successfully", "new_user": (new_user)}
    
-   try:
-      user_created= await user_to_create.create()
-
-      # Envoi de l'email en utilisant la fonction séparée
-      send_registration_email(payload.email, payload.username, pswd_not_hash)
-   except DuplicateKeyError as e:
-        error_details = e.details
-        error_message = error_details.get('errmsg', str(e))
-
-        error_info = {
-            "error_description": "Duplicate entry detected",
-            "error_message": error_message          
-        }
-        raise HTTPException(status_code=400, detail=error_info)
-
-   return {"message": "User added successfully", "id": str(user_created.id)} 
-    
-#Get all user for a pharmacy
-@router.get("/{pharmacy_id}/users",status_code=200, response_model=List[User])
-async def get_user_for_a_pharmacy(pharmacy_id: str):
-
-   #get pharmacy 
-   pharmacy= await Pharmacy.get(pharmacy_id)
-   if not pharmacy:
-      raise HTTPException(status_code=404, detail= "Pharmacy not found")
-   
-   print(pharmacy_id)
-   users= await User.find(User.pharmacy.id == ObjectId(pharmacy_id)).to_list()
-   return users
